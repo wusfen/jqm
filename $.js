64eebf971b0afc819cb43b7c8b28942e87a1e35a
wushufen <wushufen@sunlands.com>
2018-12-17 15:01:46 +0800

    w

--- $.js
+++ $.js
@@ -19,214 +19,224 @@ $('selector')
     .on() // 使用代理实现，新添加的元素也能处理事件
 
  */
-$ = function (selector) {
-    if (!(this instanceof $)) {
-        var els = new $
-        els.selector = selector
+!(function () {
 
-        if (typeof selector == 'string') {
-            var nodeList = document.querySelectorAll(selector)
-            nodeList = els.slice.call(nodeList)
-            els.splice.apply(els, [0, 0].concat(nodeList))
-        }
-        if (typeof selector == 'object') {
-            els.push(selector)
-        }
+    var $ = function (selector) {
+        if (!(this instanceof $)) {
+            var els = new $
+            els.selector = selector
 
-        return els
-    }
-}
-$.fn = $.prototype = []
-
-$.fn.delay = function (time) {
-    if (String(time).match('s')) {
-        time = parseFloat(time) * 1000
-    }
-    this.delayTime = time + (this.delayTime || 0)
-    return this
-}
-$.fn.each = function (cb) {
-    var _this = this
-    var deal = function () {
-        for (var i = 0; i < _this.length; i++) {
-            cb.call(_this[i], i, _this[i])
-        }
-        delete _this.delayTime
-    }
-
-    var delayTime = this.delayTime
-    delayTime ? setTimeout(deal, delayTime) : deal()
-
-    return this
-}
-$.fn.closest = function (selector) {
-    var _els = $(selector)
-    var els = new $
-    this.each(function () {
-        (function loop(node) {
-            if (!node) return
-            if (_els.indexOf(node) != -1 && els.indexOf(node) == -1) {
-                els.push(node)
-            } else {
-                loop(node.parentNode)
+            if (typeof selector == 'string') {
+                var nodeList = document.querySelectorAll(selector)
+                nodeList = els.slice.call(nodeList)
+                els.splice.apply(els, [0, 0].concat(nodeList))
             }
-        })(this)
-    })
-    return els
-}
-$.fn.find = function (selector) {
-    var _els = $(selector)
-    var els = new $
-    this.each(function () {
-        (function loop(node) {
-            var children = node.children
-            for (var i = 0; i < children.length; i++) {
-                var node = children[i]
+            if (typeof selector == 'object') {
+                els.push(selector)
+            }
+
+            return els
+        }
+    }
+    $.fn = $.prototype = []
+
+    $.fn.delay = function (time) {
+        if (String(time).match('s')) {
+            time = parseFloat(time) * 1000
+        }
+        this.delayTime = time + (this.delayTime || 0)
+        return this
+    }
+    $.fn.each = function (cb) {
+        var _this = this
+        var deal = function () {
+            for (var i = 0; i < _this.length; i++) {
+                cb.call(_this[i], i, _this[i])
+            }
+            delete _this.delayTime
+        }
+
+        var delayTime = this.delayTime
+        delayTime ? setTimeout(deal, delayTime) : deal()
+
+        return this
+    }
+    $.fn.closest = function (selector) {
+        var _els = $(selector)
+        var els = new $
+        this.each(function () {
+            (function loop(node) {
+                if (!node) return
                 if (_els.indexOf(node) != -1 && els.indexOf(node) == -1) {
                     els.push(node)
+                } else {
+                    loop(node.parentNode)
                 }
-                loop(node)
-            }
-        })(this)
-    })
-    return els
-}
-$.fn.parent = function () {
-    var els = new $
-    this.each(function () {
-        var node = this.parentNode
-        if (els.indexOf(node) == -1) {
-            els.push(node)
-        }
-    })
-    return els
-}
-$.fn.children = function () {
-    var els = new $
-    this.each(function () {
-        var children = this.children
-        for (var i = 0; i < children.length; i++) {
-            var node = children[i]
+            })(this)
+        })
+        return els
+    }
+    $.fn.find = function (selector) {
+        var _els = $(selector)
+        var els = new $
+        this.each(function () {
+            (function loop(node) {
+                var children = node.children
+                for (var i = 0; i < children.length; i++) {
+                    var node = children[i]
+                    if (_els.indexOf(node) != -1 && els.indexOf(node) == -1) {
+                        els.push(node)
+                    }
+                    loop(node)
+                }
+            })(this)
+        })
+        return els
+    }
+    $.fn.parent = function () {
+        var els = new $
+        this.each(function () {
+            var node = this.parentNode
             if (els.indexOf(node) == -1) {
                 els.push(node)
             }
-        }
-    })
-    return els
-}
-$.fn.addClass = function (className) {
-    var classNames = className.trim().split(/ +/)
-    return this.each(function () {
-        var classList = this.classList
-        for (var i = 0; i < classNames.length; i++) {
-            classList && classList.add(classNames[i])
-        }
-    })
-}
-$.fn.removeClass = function (className) {
-    var classNames = className.trim().split(/ +/)
-    return this.each(function () {
-        var classList = this.classList
-        for (var i = 0; i < classNames.length; i++) {
-            classList && classList.remove(classNames[i])
-        }
-    })
-}
-$.fn.css = function (arg, val) {
-    var item = this[0]
-    if (!item) return
-
-    if (val !== undefined) {
-        var key = arg
-        arg = {}, arg[key] = val
+        })
+        return els
     }
-
-    // set
-    if (typeof arg == 'object') {
+    $.fn.children = function () {
+        var els = new $
+        this.each(function () {
+            var children = this.children
+            for (var i = 0; i < children.length; i++) {
+                var node = children[i]
+                if (els.indexOf(node) == -1) {
+                    els.push(node)
+                }
+            }
+        })
+        return els
+    }
+    $.fn.addClass = function (className) {
+        var classNames = className.trim().split(/ +/)
         return this.each(function () {
-            var style = this.style
-            for (var name in arg) {
-                style['-webkit-' + name] = arg[name]
-                style['-moz-' + name] = arg[name]
-                style['-ms-' + name] = arg[name]
-                style['-0-' + name] = arg[name]
-                style[name] = arg[name]
+            var classList = this.classList
+            for (var i = 0; i < classNames.length; i++) {
+                classList && classList.add(classNames[i])
             }
         })
     }
-    // get
-    else {
-        var style = getComputedStyle(item, null)
-        var value =
-            style['-webkit-' + arg] ||
-            style['-moz-' + arg] ||
-            style['-ms-' + arg] ||
-            style['-o-' + arg] ||
-            style[arg]
+    $.fn.removeClass = function (className) {
+        var classNames = className.trim().split(/ +/)
+        return this.each(function () {
+            var classList = this.classList
+            for (var i = 0; i < classNames.length; i++) {
+                classList && classList.remove(classNames[i])
+            }
+        })
+    }
+    $.fn.css = function (arg, val) {
+        var item = this[0]
+        if (!item) return
 
-        return arg ? value : style
+        if (val !== undefined) {
+            var key = arg
+            arg = {}, arg[key] = val
+        }
+
+        // set
+        if (typeof arg == 'object') {
+            return this.each(function () {
+                var style = this.style
+                for (var name in arg) {
+                    style['-webkit-' + name] = arg[name]
+                    style['-moz-' + name] = arg[name]
+                    style['-ms-' + name] = arg[name]
+                    style['-0-' + name] = arg[name]
+                    style[name] = arg[name]
+                }
+            })
+        }
+        // get
+        else {
+            var style = getComputedStyle(item, null)
+            var value =
+                style['-webkit-' + arg] ||
+                style['-moz-' + arg] ||
+                style['-ms-' + arg] ||
+                style['-o-' + arg] ||
+                style[arg]
+
+            return arg ? value : style
+        }
+
+    }
+    $.fn.show = function (className) {
+        this.each(function () {
+            $(this).css('display', '')
+            if ($(this).css('display') == 'none') {
+                $(this).css('display', 'block')
+            }
+        })
+        if (className) {
+            this.addClass(className)
+                .delay(this.css('animation-duration'))
+                .removeClass(className)
+        }
+        return this
+    }
+    $.fn.hide = function (className) {
+        if (className) {
+            this.addClass(className)
+                .delay(this.css('animation-duration'))
+                .removeClass(className)
+        }
+        return this.css('display', 'none')
+    }
+    $.fn.html = function (value) {
+        if (arguments.length) {
+            this.each(function () {
+                this.innerHTML = value
+            })
+        } else {
+            return this[0] ? this[0].innerHTML : ''
+        }
+        return this
+    }
+    $.fn.text = function (value) {
+        if (arguments.length) {
+            this.each(function () {
+                this.innerText = value
+            })
+        } else {
+            return this[0] ? this[0].innerText : ''
+        }
+        return this
+    }
+    $.fn.attr = function (name, value) {
+        if (arguments.length > 1) {
+            this.each(function () {
+                this.setAttribute(name, value)
+            })
+        } else {
+            return this[0] ? this[0].getAttribute(name) : null
+        }
+        return this
+    }
+    $.fn.on = function (eventType, cb) {
+        var _this = this
+        document.addEventListener(eventType, function (e) {
+            var el = $(e.target).closest(_this.selector)[0]
+            if (el) {
+                cb.call(el, e)
+            }
+        })
+        return this
     }
 
-}
-$.fn.show = function (className) {
-    this.each(function () {
-        $(this).css('display', '')
-        if ($(this).css('display') == 'none') {
-            $(this).css('display', 'block')
-        }
-    })
-    if (className) {
-        this.addClass(className)
-            .delay(this.css('animation-duration'))
-            .removeClass(className)
-    }
-    return this
-}
-$.fn.hide = function (className) {
-    if (className) {
-        this.addClass(className)
-            .delay(this.css('animation-duration'))
-            .removeClass(className)
-    }
-    return this.css('display', 'none')
-}
-$.fn.html = function (value) {
-    if (arguments.length) {
-        this.each(function () {
-            this.innerHTML = value
-        })
+    if (typeof module != 'undefined') {
+        module.exports = $
     } else {
-        return this[0] ? this[0].innerHTML : ''
+        window.$ = $
     }
-    return this
-}
-$.fn.text = function (value) {
-    if (arguments.length) {
-        this.each(function () {
-            this.innerText = value
-        })
-    } else {
-        return this[0] ? this[0].innerText : ''
-    }
-    return this
-}
-$.fn.attr = function (name, value) {
-    if (arguments.length > 1) {
-        this.each(function () {
-            this.setAttribute(name, value)
-        })
-    } else {
-        return this[0] ? this[0].getAttribute(name) : null
-    }
-    return this
-}
-$.fn.on = function (eventType, cb) {
-    var _this = this
-    document.addEventListener(eventType, function (e) {
-        var el = $(e.target).closest(_this.selector)[0]
-        if (el) {
-            cb.call(el, e)
-        }
-    })
-    return this
-}
+
+})()
---
+++ index.js
@@ -0,0 +1,2 @@
+var $ = require('./$')
+console.log($)
